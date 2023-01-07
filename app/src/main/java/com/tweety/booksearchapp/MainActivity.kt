95b74821ea46fffc15dc3eae25260e9d0592de73
kyj <flysoar3029@gmail.com>
2023-01-07 22:11:12 +0900

    [Add] 바텀 네비게이션과 3개의 프래그먼트 전환 구현

--- app/src/main/java/com/tweety/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/tweety/booksearchapp/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.tweety.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tweety.booksearchapp.databinding.ActivityMainBinding
+import com.tweety.booksearchapp.ui.FavoriteFragment
+import com.tweety.booksearchapp.ui.SearchFragment
+import com.tweety.booksearchapp.ui.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+        // 앱 처음 실행시에만 바텀네비게이션 바인딩
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
