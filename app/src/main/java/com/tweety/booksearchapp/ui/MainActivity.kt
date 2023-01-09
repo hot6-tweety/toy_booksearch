a67afb3ee2ac5ef3aa261081927dd8827187fe12
kyj <flysoar3029@gmail.com>
2023-01-10 00:28:58 +0900

    [Update] JetPack Navigation 으로 화면 전환

--- app/src/main/java/com/tweety/booksearchapp/ui/MainActivity.kt
+++ app/src/main/java/com/tweety/booksearchapp/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.tweety.booksearchapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.tweety.booksearchapp.R
 import com.tweety.booksearchapp.data.repository.BookSearchRepositoryImpl
 import com.tweety.booksearchapp.databinding.ActivityMainBinding
@@ -20,41 +22,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupBottomNavigationView()
-        // 앱 처음 실행시에만 바텀네비게이션 바인딩
-        if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
-        }
+        setupBottomNavigation()
 
         val bookSearchRepository = BookSearchRepositoryImpl()
         val factory = ViewModelFactory(bookSearchRepository, this)
         bookSearchViewModel = ViewModelProvider(this, factory)[BookSearchViewModel::class.java]
     }
 
-    private fun setupBottomNavigationView() {
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.fragment_search -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SearchFragment())
-                        .commit()
-                    true
+    private fun setupBottomNavigation() {
+        val navController = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment)?.findNavController()
 
-                }
-                R.id.fragment_favorite -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, FavoriteFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_settings -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SettingsFragment())
-                        .commit()
-                    true
-                }
-                else -> false
-            }
+        navController?.let {
+            binding.bottomNavigationView.setupWithNavController(navController)
         }
+
+
     }
 }
